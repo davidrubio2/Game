5f886de547d5884d7466359893e702df58f9c062
davidrubio2 <davidrubio2@gmail.com>
2018-07-25 23:41:13 -0600

    add a sample menu play and settings, detect touch menu and add detect back key

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -1,20 +1,23 @@
 package com.mygdx.game;
-import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
-import com.badlogic.gdx.graphics.g2d.Sprite;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.math.MathUtils;
-import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.utils.Array;
-import com.badlogic.gdx.utils.Scaling;
 import com.badlogic.gdx.utils.TimeUtils;
+import com.badlogic.gdx.utils.viewport.ExtendViewport;
 import com.badlogic.gdx.utils.viewport.FillViewport;
 import com.badlogic.gdx.utils.viewport.ScalingViewport;
 
@@ -22,6 +25,8 @@ import java.util.Iterator;
 
 public class MyGdxGame extends ApplicationAdapter {
 
+    public enum GameState{MENU,PLAY,SETTINGS}
+    public static GameState eGameState;
     private int iwidthScreen = 480;
     private int iheightScreen = 800;
     private Texture tBackImage;
@@ -34,10 +39,18 @@ public class MyGdxGame extends ApplicationAdapter {
     private ScalingViewport viewport;
     private int score;
     private String yourScoreName;
-    BitmapFont yourBitmapFontName;
-	
+    private  BitmapFont yourBitmapFontName;
+
+    private Stage stage;
+    private TextButton button;
+    private TextButton.TextButtonStyle textButtonStyle;
+    private BitmapFont font;
+    TextButton button1;
+    TextButton button2;
+
 	@Override
 	public void create () {
+        Gdx.input.setCatchBackKey(true);
 
         score = 0;
         yourScoreName = "score: 0";
@@ -54,6 +67,27 @@ public class MyGdxGame extends ApplicationAdapter {
         viewport = new FillViewport(iwidthScreen,iheightScreen,camera);
         viewport.apply();
 
+        eGameState = GameState.MENU;
+
+        stage = new Stage(new ExtendViewport(iwidthScreen,iheightScreen));
+        Gdx.input.setInputProcessor(stage);
+
+        Table table = new Table();
+        table.setFillParent(true);
+         stage.addActor(table);
+        font = new BitmapFont();
+        textButtonStyle = new TextButton.TextButtonStyle();
+        textButtonStyle.font = font;
+
+        button1  = new TextButton("Play", textButtonStyle);
+        button2 = new TextButton("Settings", textButtonStyle);
+
+        table.add(button1).space(50);
+        table.row();
+
+        table.add(button2).space(50);
+        table.row();
+
 
     }
 
@@ -67,25 +101,75 @@ public class MyGdxGame extends ApplicationAdapter {
 		lLastBalloonTime = TimeUtils.nanoTime();
 	 }
 
+    @Override
+    public void pause(){
+        eGameState = GameState.MENU;
+    }
+
+
+
+    @Override
+    public void resume()
+    {
+        eGameState = GameState.PLAY;
+    }
+
 	@Override
 	public void render () {
-        Gdx.gl.glClearColor(1, 0, 0, 1);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        camera.update();
-        batch.setProjectionMatrix(camera.combined);
+        switch (eGameState) {
+            case MENU:
+                Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+                camera.update();
+                batch.setProjectionMatrix(camera.combined);
+                stage.act(Gdx.graphics.getDeltaTime());
+                stage.draw();
+                batch.begin();
+                batch.end();
+                button1.addListener(new ChangeListener() {
+                    @Override
+                    public void changed(ChangeEvent event, Actor actor) {
+                        eGameState = GameState.PLAY;
+                    }
+                });
 
-        batch.begin();
-        batch.draw(tBackImage, 0,0,iwidthScreen,iheightScreen);
-        yourBitmapFontName.setColor(1.0f, 1.0f, 1.0f, 1.0f);
-        yourBitmapFontName.draw(batch, yourScoreName, 25, 100); 
-        for (Rectangle rBalloon : aBalloons) {
-            batch.draw(tBalloons, rBalloon.x, rBalloon.y);
-         }
+                Gdx.input.setInputProcessor(stage);
+
+                break;
+            case PLAY:
+                if (Gdx.input.isKeyPressed(Input.Keys.BACK)) {
+                    pause();
+                }
+                Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+                camera.update();
+                batch.setProjectionMatrix(camera.combined);
+
+                batch.begin();
+                batch.draw(tBackImage, 0, 0, iwidthScreen, iheightScreen);
+                yourBitmapFontName.setColor(1.0f, 1.0f, 1.0f, 1.0f);
+                yourBitmapFontName.draw(batch, yourScoreName, 25, 100);
+                for (Rectangle rBalloon : aBalloons) {
+                    batch.draw(tBalloons, rBalloon.x, rBalloon.y);
+                }
+                batch.end();
+                logica();
+                break;
+            case SETTINGS:
+                break;
+
+        }
+
+    }
+	
+	@Override
+	public void dispose () {
+		batch.dispose();
+		tBackImage.dispose();
+       stage.dispose();
+	}
 
 
-        batch.end();
-
-
+	public void logica()
+    {
         if (TimeUtils.nanoTime() - lLastBalloonTime > 1000000000) spawnBalloons();
 
         for (Iterator<Rectangle> iter = aBalloons.iterator(); iter.hasNext(); ) {
@@ -94,8 +178,6 @@ public class MyGdxGame extends ApplicationAdapter {
             rBalloon.y += 100 * Gdx.graphics.getDeltaTime();
 
             rBalloon.x -= 10 * Gdx.graphics.getDeltaTime();
-
-
 
             for (int i = 0; i < 10; i++) {
                 if (Gdx.input.isTouched(i)) {
@@ -119,17 +201,15 @@ public class MyGdxGame extends ApplicationAdapter {
                 iter.remove();
             }
         }
+
     }
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		tBackImage.dispose();
-	}
 
     @Override
     public void resize(int width, int height){
         viewport.update(width,height);
-       camera.position.set(camera.viewportWidth/2,camera.viewportHeight/2,0);
+        camera.position.set(camera.viewportWidth/2,camera.viewportHeight/2,0);
+        stage.getViewport().update(width, height, true);
     }
+
+
 }
